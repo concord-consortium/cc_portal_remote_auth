ea7bde52819bd1282363b8f2213bb901e0c84540
Aaron Unger <aaron.unger@gmail.com>
2013-07-26 14:24:51 -0700

    Functionality for logging in remotely, verifying authentication token

---
+++ app/controllers/remote_auth_controller.rb
@@ -0,0 +1,51 @@
+class RemoteAuthController < ApplicationController
+  # verify a CC token
+  def verify_cc_token
+    begin
+      token = cookies[CCCookieAuth.cookie_name]
+      raise 'non-existent token' unless token
+      valid = CCCookieAuth.verify_auth_token(token,request.remote_ip)
+      raise 'invalid token' unless valid
+      login = token.split(CCCookieAuth.token_separator).first
+      raise 'token parse error' unless login
+      user = User.find_by_login(login)
+      raise 'bogus user' unless user
+      values = {:login => login, :first => user.first_name, :last => user.last_name}
+      student = user.portal_student
+      teacher = user.portal_teacher
+      # values[:group] = user.group_account_class_id ? true : false
+      if student
+        values[:class_words] = student.clazzes.map{ |c| c.class_word }
+        values[:teacher] = false
+        values[:classes] = student.clazzes.map{|c|
+          cohorts = c.teachers.map{|t| t.cohorts}.flatten.compact.uniq
+          val = {:teacher => (c.teacher.name rescue "unknown"), :word => c.class_word, :name => c.name, :cohorts => cohorts}
+          offerings = c.offerings
+          offerings = offerings.select{|o| o.active && o.runnable.is_a?(ExternalActivity)}
+          offerings = offerings.select{|o|
+            runnable_url = o.runnable.url.gsub(/\/\s*$/,'')
+            save_path = o.runnable.save_path.gsub(/\/\s*$/,'')
+            referrer = request.referrer.gsub(/\/\s*$/,'')
+            town_level_referrer = referrer.gsub(/(\?task=(?:baseline\/)?\d+)\/\d+$/) {|m| $1 }
+            runnable_url == referrer || save_path == referrer || save_path == town_level_referrer
+          }
+          offering = offerings.first
+          if offering # what do we do if somehow multiple external activities with the same url are assigned to the same class??
+            learner = offering.find_or_create_learner(student)
+            val[:learner] = learner.id if learner
+          end
+          val
+        }
+        values[:cohorts] = values[:classes].map{|vc| vc[:cohorts]}.flatten.compact.uniq.map{|c| c.name}
+      end
+      if teacher
+        values[:class_words] = teacher.clazzes.map{ |c| c.class_word }
+        values[:cohorts] = teacher.cohorts.map{|c| c.name }
+        values[:teacher] = true
+      end
+      render :json => values
+    rescue Exception => e
+      render :text => "authentication failure: #{e.message}", :status => 403
+    end
+  end
+end
--- lib/cc_portal_remote_auth.rb
+++ lib/cc_portal_remote_auth.rb
@@ -1,4 +1,39 @@
 require "cc_portal_remote_auth/engine"
+require "cgi"
+require "cc_cookie_auth"
 
 module CcPortalRemoteAuth
+  Devise::Strategies::Authenticatable.class_eval do
+    def params_auth_hash_with_remote
+      if params[:login] && params[:password]
+        return { :login => params[:login], :password => params[:password] }
+      end
+      return params_auth_hash_without_remote
+    end
+    alias_method_chain :params_auth_hash, :remote
+  end
+
+  Warden::Manager.after_authentication do |user, warden, options|
+    cookies = warden.cookies
+    request = warden.request
+    token = CCCookieAuth.make_auth_token(user.login, request.remote_ip)
+    cookies[CCCookieAuth.cookie_name.to_sym] = {:value => token, :domain => cookie_domain(request) }
+  end
+
+  Warden::Manager.before_logout do |user, warden, options|
+    cookies = warden.cookies
+    if cookies.kind_of? ActionDispatch::Cookies::CookieJar
+      cookies.delete(CCCookieAuth.cookie_name.to_sym, {:domain => cookie_domain})
+    else
+      cookies.delete CCCookieAuth.cookie_name.to_sym
+    end
+  end
+
+  def self.cookie_domain(request)
+    # use wildcard domain (last two parts ".concord.org") for this cookie
+    cookie_domain = request.host
+    cookie_domain = '.concord.org' if cookie_domain =~ /\.concord\.org$/
+
+    return cookie_domain
+  end
 end
