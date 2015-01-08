7618fb0462577da07f6ca7206707b709045c9ae1
Aaron Unger <aaron.unger@gmail.com>
2015-01-08 14:41:00 -0800

    Don't cache verify_cc_token responses

--- app/controllers/remote_auth_controller.rb
+++ app/controllers/remote_auth_controller.rb
@@ -1,6 +1,10 @@
 class RemoteAuthController < ApplicationController
   # verify a CC token
   def verify_cc_token
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+
     begin
       token = cookies[CCCookieAuth.cookie_name]
       raise 'non-existent token' unless token
